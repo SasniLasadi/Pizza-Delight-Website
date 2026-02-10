60f47c4326ae45a74ddfe5606ab6baa40317903c
Sasni Lasadi <sasnilasadi410@gmail.com>
2026-02-10 22:33:08 +0530

    Add reorder icon to improve navbar responsiveness

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";      
 import Logo from "../assets/pizzaLogo.png";
 import {Link} from "react-router-dom";
+import ReorderIcon from '@mui/icons-material/Reorder';
 import  "../styles/Navbar.css";
 
 function Navbar() {
@@ -15,6 +16,9 @@ function Navbar() {
         <Link to="/menu">Menu</Link>
         <Link to="/about">About</Link>
         <Link to="/contact">Contact</Link>
+        <button>
+          <ReorderIcon />
+        </button>
       </div>
     </div>
   )
